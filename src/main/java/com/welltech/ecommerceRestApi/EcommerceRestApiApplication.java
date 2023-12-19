e3cdcad63c8e13cd44b926d3ca9cd51a11603cb0
Cliff <cagyei529@gmail.com>
2023-12-19 10:24:59 +0000

    alpha commit

---
+++ src/main/java/com/welltech/ecommerceRestApi/EcommerceRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.welltech.ecommerceRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceRestApiApplication.class, args);
+	}
+
+}
